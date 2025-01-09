b437b988e18df54099a290eb3d2b56fbad493d39
YannickMyxe <shawn.froste.sf@gmail.com>
2025-01-09 11:26:50 +0100

    Discord bot now loads successfully

---
+++ Commands/CommandHelper.cs
@@ -0,0 +1,5 @@
+﻿namespace DiscordBot.Commands;
+
+public class CommandHelper {
+    
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using Discord;
+using Discord.WebSocket;
+using DiscordBot;
+
+class Program {
+    
+    private static DiscordSocketClient client;
+    
+    public static async Task Main() {
+        client = new DiscordSocketClient();
+        client.Log += Log;
+        
+        var path = "../../../.env";
+        EnvReader.Load(path);
+        
+        var token = Environment.GetEnvironmentVariable("DISCORD_TOKEN");
+        if (token is null) throw new Exception("Token is missing");
+        
+        await client.LoginAsync(TokenType.Bot, token);
+        await client.StartAsync();
+
+        // Block this task until the program is closed.
+        await Task.Delay(-1);
+    }
+    
+    private static Task Log(LogMessage msg) {
+        Console.WriteLine(msg.ToString());
+        return Task.CompletedTask;
+    }
+
+}
